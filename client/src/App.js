88212f3cd22efeba9ec31e8d7b2bedac451b62bf
mlluciano <85596570+mlluciano@users.noreply.github.com>
2022-01-09 13:11:51 -0500

    Setup top banner and left navigation bar.
    HTML/CSS was written to organize the top banner and left navigation bar.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='banner'> 
+        <h1>Life<span>West</span></h1>
+      </div>
+      <div className='nav'>
+        <a href='#'>Home</a>
+        <a href='#'>Register</a>
+        <a href='#'>Donor Lookup</a>
+        <a href='#'>Orders</a>
+        <a href='#'>Inventory</a>
+        
+      </div>
     </div>
   );
 }
